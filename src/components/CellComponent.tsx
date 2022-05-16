0ccc6b5d0b245f6b3b2fde49b05f30bac66f0d91
suveiko <suveikosasha@gmail.com>
2022-05-16 17:35:09 +0400

    made a chessboard

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import {Cell} from "../models/Cell";
+
+interface  CellProps {
+    cell: Cell
+}
+
+export const CellComponent: FC<CellProps> = ({cell}) => {
+    return (
+        <div className={['cell', cell.color].join(' ')}>
+        </div>
+    );
+};
