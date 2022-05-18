848c1e4e5f21ef9ce31ff2faac75e1f53da2f665
suveiko <suveikosasha@gmail.com>
2022-05-18 10:56:45 +0400

    Restart the game. Pass a move

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -2,22 +2,27 @@ import React, {FC, useState} from 'react';
 import {Board} from "../models/Board";
 import {CellComponent} from "./CellComponent";
 import {Cell} from "../models/Cell";
+import {Player} from "../models/Player";
 
 interface BoardProps {
     board: Board
     setBoard: (board: Board) => void
+    currentPlayer: Player | null
+    swapPlayer: () => void
 }
 
-export const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+export const BoardComponent: FC<BoardProps> = ({board, setBoard, currentPlayer, swapPlayer}) => {
     const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
 
     const click = (cell: Cell) => {
         if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
             selectedCell.moveFigure(cell)
+            swapPlayer()
             setSelectedCell(null)
             updateBoard()
         } else {
-            setSelectedCell(cell)
+            if (cell.figure?.color === currentPlayer?.color)
+                setSelectedCell(cell)
         }
     }
 
@@ -30,20 +35,25 @@ export const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
         const newBoard = board.getCopyBoard()
         setBoard(newBoard)
     }
+
     return (
-        <div className='board'>
-            {board.cells.map((row, index) =>
-                <React.Fragment key={index}>
-                    {row.map(cell =>
-                        <CellComponent
-                            click={click}
-                            cell={cell}
-                            key={cell.id}
-                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
-                        />
-                    )}
-                </React.Fragment>
-            )}
+        <div>
+            <h3>Current player {currentPlayer?.color}</h3>
+            <div className='board'>
+                {board.cells.map((row, index) =>
+                    <React.Fragment key={index}>
+                        {row.map(cell =>
+                            <CellComponent
+                                click={click}
+                                cell={cell}
+                                key={cell.id}
+                                selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                            />
+                        )}
+                    </React.Fragment>
+                )}
+            </div>
         </div>
+
     );
 };
--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -5,7 +5,6 @@ import blackLogo from "../../assets/black-king.png";
 import whiteLogo from "../../assets/white-king.png";
 
 export class King extends Figure {
-
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo
